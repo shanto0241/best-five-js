46f03e0a2ca7c4d6f1d19a929c1aa9e39be46cd6
Sudipta Sinha Shanta <shantonu2050@gmail.com>
2022-08-23 06:43:31 +0600

    added player button event listener

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// player buttons event listeners
+document.getElementById("player-1").addEventListener("click", function () {
+  const clickSelected = buttonClicked("neymar");
+  getSelectedElement(clickSelected, "player-1");
+});
+document.getElementById("player-2").addEventListener("click", function () {
+  const clickSelected = buttonClicked("messi");
+  getSelectedElement(clickSelected, "player-2");
+});
+document.getElementById("player-3").addEventListener("click", function () {
+  const clickSelected = buttonClicked("ronaldo");
+  getSelectedElement(clickSelected, "player-3");
+});
+document.getElementById("player-4").addEventListener("click", function () {
+  const clickSelected = buttonClicked("oreo");
+  getSelectedElement(clickSelected, "player-4");
+});
+document.getElementById("player-5").addEventListener("click", function () {
+  const clickSelected = buttonClicked("ozil");
+  getSelectedElement(clickSelected, "player-5");
+});
+document.getElementById("player-6").addEventListener("click", function () {
+  const clickSelected = buttonClicked("salah");
+  getSelectedElement(clickSelected, "player-6");
+});
+document.getElementById("player-7").addEventListener("click", function () {
+  const clickSelected = buttonClicked("wayne");
+  getSelectedElement(clickSelected, "player-7");
+});
+document.getElementById("player-8").addEventListener("click", function () {
+  const clickSelected = buttonClicked("obeuch");
+  getSelectedElement(clickSelected, "player-8");
+});
+document.getElementById("player-9").addEventListener("click", function () {
+  const clickSelected = buttonClicked("alberto");
+  getSelectedElement(clickSelected, "player-9");
+});
